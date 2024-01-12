482688484cc4875cf358565f9d074122b81cc6f1
Djemal Bimov <150387781+djemalbimov@users.noreply.github.com>
2024-01-12 17:21:22 +0200

    Lecture-04-js-fundamentals-data-types-and-variables-exercise
    - sumDigits
    - charsToString
    - townInfo
    - convertMetersToKilometres
    - poundsToDollars
    - reversedChars
    - lowerOrUpper

---
+++ LectureFour/03-townInfo.js
@@ -0,0 +1,8 @@
+function townInfo(name, pop, area) {
+
+    console.log(`Town ${name} has population of ${pop} and area ${area} square km.`);
+    
+}
+
+townInfo('Sofia' ,1286383, 492);
+townInfo('Plovdiv',1481353,512);
---
+++ LectureFour/04-convertMetersToKilometres.js
@@ -0,0 +1,12 @@
+function convertMetersToKilometres(meters) {
+
+    let kms = meters / 1000;
+
+    console.log(kms.toFixed(2));
+
+}
+
+convertMetersToKilometres(1852);
+convertMetersToKilometres(798);
+convertMetersToKilometres(80);
+convertMetersToKilometres(39);
